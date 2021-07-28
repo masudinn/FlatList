b1db792feb06c56755a885ba0e914634b9a57947
masudinn <fachrudin.2000@students.amikom.ac.id>
2021-07-28 07:40:11 +0700

    mengatur package agar rapi

---
+++ page/FlatListHorizontal/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function FlatListHorizontal() {
+    return (
+        <View>
+            <Text>asd</Text>
+        </View>
+    )
+}
